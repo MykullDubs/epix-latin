7029bd46d0548699ce1ab60605d6dc8346499a91
MykullDubs <142657467+MykullDubs@users.noreply.github.com>
2026-03-03 17:20:33 -0700

    Update curriculums.ts

--- src/constants/curriculums.ts
+++ src/constants/curriculums.ts
@@ -20,9 +20,13 @@ export const GLOBAL_CURRICULUMS: Curriculum[] = [
     themeColor: '#3b82f6', // blue
     lessonIds: [
       'lesson_a1_01', 'lesson_a1_02', 'lesson_a1_03', 'lesson_a1_04', 'lesson_a1_05',
+      'exam_a1_01', // Sprint 1 Checkpoint
       'lesson_a1_06', 'lesson_a1_07', 'lesson_a1_08', 'lesson_a1_09', 'lesson_a1_10',
+      'exam_a1_02', // Sprint 2 Checkpoint (Midterm)
       'lesson_a1_11', 'lesson_a1_12', 'lesson_a1_13', 'lesson_a1_14', 'lesson_a1_15',
-      'lesson_a1_16', 'lesson_a1_17', 'lesson_a1_18', 'lesson_a1_19', 'lesson_a1_20'
+      'exam_a1_03', // Sprint 3 Checkpoint
+      'lesson_a1_16', 'lesson_a1_17', 'lesson_a1_18', 'lesson_a1_19', 'lesson_a1_20',
+      'exam_a1_mastery' // Tier Boss Battle
     ]
   },
   {
@@ -34,9 +38,13 @@ export const GLOBAL_CURRICULUMS: Curriculum[] = [
     themeColor: '#10b981', // emerald
     lessonIds: [
       'lesson_a2_01', 'lesson_a2_02', 'lesson_a2_03', 'lesson_a2_04', 'lesson_a2_05',
+      'exam_a2_01', 
       'lesson_a2_06', 'lesson_a2_07', 'lesson_a2_08', 'lesson_a2_09', 'lesson_a2_10',
+      'exam_a2_02', 
       'lesson_a2_11', 'lesson_a2_12', 'lesson_a2_13', 'lesson_a2_14', 'lesson_a2_15',
-      'lesson_a2_16', 'lesson_a2_17', 'lesson_a2_18', 'lesson_a2_19', 'lesson_a2_20'
+      'exam_a2_03', 
+      'lesson_a2_16', 'lesson_a2_17', 'lesson_a2_18', 'lesson_a2_19', 'lesson_a2_20',
+      'exam_a2_mastery' 
     ]
   },
   {
@@ -48,9 +56,13 @@ export const GLOBAL_CURRICULUMS: Curriculum[] = [
     themeColor: '#8b5cf6', // violet
     lessonIds: [
       'lesson_b1_01', 'lesson_b1_02', 'lesson_b1_03', 'lesson_b1_04', 'lesson_b1_05',
+      'exam_b1_01', 
       'lesson_b1_06', 'lesson_b1_07', 'lesson_b1_08', 'lesson_b1_09', 'lesson_b1_10',
+      'exam_b1_02', 
       'lesson_b1_11', 'lesson_b1_12', 'lesson_b1_13', 'lesson_b1_14', 'lesson_b1_15',
-      'lesson_b1_16', 'lesson_b1_17', 'lesson_b1_18', 'lesson_b1_19', 'lesson_b1_20'
+      'exam_b1_03', 
+      'lesson_b1_16', 'lesson_b1_17', 'lesson_b1_18', 'lesson_b1_19', 'lesson_b1_20',
+      'exam_b1_mastery' 
     ]
   },
   {
@@ -62,9 +74,13 @@ export const GLOBAL_CURRICULUMS: Curriculum[] = [
     themeColor: '#f59e0b', // amber
     lessonIds: [
       'lesson_b2_01', 'lesson_b2_02', 'lesson_b2_03', 'lesson_b2_04', 'lesson_b2_05',
+      'exam_b2_01', 
       'lesson_b2_06', 'lesson_b2_07', 'lesson_b2_08', 'lesson_b2_09', 'lesson_b2_10',
+      'exam_b2_02', 
       'lesson_b2_11', 'lesson_b2_12', 'lesson_b2_13', 'lesson_b2_14', 'lesson_b2_15',
-      'lesson_b2_16', 'lesson_b2_17', 'lesson_b2_18', 'lesson_b2_19', 'lesson_b2_20'
+      'exam_b2_03', 
+      'lesson_b2_16', 'lesson_b2_17', 'lesson_b2_18', 'lesson_b2_19', 'lesson_b2_20',
+      'exam_b2_mastery' 
     ]
   },
   {
@@ -76,9 +92,13 @@ export const GLOBAL_CURRICULUMS: Curriculum[] = [
     themeColor: '#f43f5e', // rose
     lessonIds: [
       'lesson_c1_01', 'lesson_c1_02', 'lesson_c1_03', 'lesson_c1_04', 'lesson_c1_05',
+      'exam_c1_01', 
       'lesson_c1_06', 'lesson_c1_07', 'lesson_c1_08', 'lesson_c1_09', 'lesson_c1_10',
+      'exam_c1_02', 
       'lesson_c1_11', 'lesson_c1_12', 'lesson_c1_13', 'lesson_c1_14', 'lesson_c1_15',
-      'lesson_c1_16', 'lesson_c1_17', 'lesson_c1_18', 'lesson_c1_19', 'lesson_c1_20'
+      'exam_c1_03', 
+      'lesson_c1_16', 'lesson_c1_17', 'lesson_c1_18', 'lesson_c1_19', 'lesson_c1_20',
+      'exam_c1_mastery' 
     ]
   }
 ];
