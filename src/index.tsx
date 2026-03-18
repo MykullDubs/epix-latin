0b603e87a39efc3da44869c2c39a7971c0d23b42
MykullDubs <142657467+MykullDubs@users.noreply.github.com>
2026-03-17 18:09:56 -0600

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// 🔥 WAKE UP THE PWA ENGINE
-// @ts-ignore
-import { registerSW } from 'virtual:pwa-register';
-
-// Start the service worker immediately so it caches the app
-registerSW({ immediate: true });
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
