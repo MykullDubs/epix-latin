f3d550cd7211b812be75cbf0c98d07af79eeb504
MykullDubs <142657467+MykullDubs@users.noreply.github.com>
2026-07-25 08:28:41 -0600

    Refactor import paths for InstructorDashboard and BuilderHub

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,9 @@ import {
 import PrepositionExam from './components/PrepositionExam';
 import LandingPage from './components/LandingPage';
 import StudentClassView from './components/StudentClassView';
-import InstructorDashboard from './components/InstructorDashboard';
+import InstructorDashboard from './components/instructor/InstructorDashboard';
 import FlashcardView from './components/FlashcardView';
-import BuilderHub from './components/BuilderHub';
+import BuilderHub from './components/instructor/BuilderHub';
 import ProfileView from './components/ProfileView';
 import AuthView from './components/AuthView'; // If you also have AuthView separate
 // import AuthView from './components/AuthView'; // Uncomment if AuthView is in a separate file
